0b8c9ccb1eae7f76f3f1bc3d1c3eb4ff152f5897
Mark <mark@rowra.org>
2026-07-02 16:44:46 +0000

    .

--- components/sinclair_ac/esppac.h
+++ components/sinclair_ac/esppac.h
@@ -23,20 +23,21 @@ static const float TEMPERATURE_STEP = 1.0;   // Steps the temperature can be set
 static const float TEMPERATURE_TOLERANCE = 2;  // The tolerance to allow when checking the climate state
 static const uint8_t TEMPERATURE_THRESHOLD = 100;  // Maximum temperature the AC can report (formally 119.5 for sinclair protocol, but 100 is impossible, soo...)
 
+/* Fan modes setup - because different units may use slightly different values
+*/
 struct FanModeConfig {
     const char* name;
     int sp1;
     int sp2;
 };
 
-// 2. Wrap them up in your namespace as constexpr instances
 namespace fan_modes {
-    constexpr FanModeConfig FAN_AUTO     = { "auto",     8, 0 };
-    constexpr FanModeConfig FAN_LOW      = { "low",      9, 1 };
-    constexpr FanModeConfig FAN_LOWMED   = { "low-medium",   10, 2 };
-    constexpr FanModeConfig FAN_MED      = { "medium",   11, 2 };
-    constexpr FanModeConfig FAN_MEDHIGH  = { "medium-high",  12, 3 };
-    constexpr FanModeConfig FAN_HIGH     = { "high",     13, 3 };
+    constexpr FanModeConfig FAN_AUTO     = { "0 - Auto",     8, 0 };
+    constexpr FanModeConfig FAN_LOW      = { "1 - Low",      9, 1 };
+    constexpr FanModeConfig FAN_LOWMED   = { "2 - Low-medium",   10, 2 };
+    constexpr FanModeConfig FAN_MED      = { "3 - Medium",   11, 2 };
+    constexpr FanModeConfig FAN_MEDHIGH  = { "4 - Medium-high",  12, 3 };
+    constexpr FanModeConfig FAN_HIGH     = { "5 - High",     13, 3 };
 
     constexpr std::array<FanModeConfig, 6> ALL_MODES = {
         FAN_AUTO, FAN_LOW, FAN_LOWMED, FAN_MED, FAN_MEDHIGH, FAN_HIGH
